ed203035331c64b1d31022f288dcff15eb75df94
manish <manishengi25@gmail.com>
2015-07-28 19:08:28 +0530

    Core file added

---
+++ src/wordfrequency/core.clj
@@ -0,0 +1,22 @@
+(ns wordfrequancy.core 
+(:gen-class))
+
+(ns tokenize
+ (:import (java.io BufferedWriter FileWriter)))
+
+(defn split-words [text]
+ "split text into a list of words"
+ (re-seq #"\w+" text))
+  
+(defn printwords [words-hash]
+ (map (fn [[a b]] (str b ": " a "\n")) words-hash))
+
+(defn write-to-file [file-name lines]
+ (with-open [wtr (BufferedWriter. (FileWriter. file-name))]
+  (doseq [line lines] (.write wtr line))))
+
+(prn (write-to-file "output.txt"
+   (printwords
+     (sort-by val
+     (frequencies(split-words
+      (clojure.string/lower-case (slurp "test.txt"))))))))
