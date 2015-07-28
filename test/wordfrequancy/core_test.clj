6df2e8e2e5c2d146fb817135036c462a321c1d84
manish <manishengi25@gmail.com>
2015-07-28 16:22:01 +0530

    Initial Commit

---
+++ test/wordfrequancy/core_test.clj
@@ -0,0 +1,7 @@
+(ns wordfrequancy.core-test
+  (:require [clojure.test :refer :all]
+            [wordfrequancy.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
